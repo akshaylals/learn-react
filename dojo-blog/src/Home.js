10bf097be0ba0296b7bd9ebbcbe206a1c8021432
akshaylals <akshaylal.s@inapp.com>
2022-10-21 11:08:27 +0530

    error catching

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,21 +4,33 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(response => response.json())
-            .then((data) => {
-                console.log(data);
-                setBlogs(data)
-                setIsLoading(false);
-            })
+                .then(response => {
+                    if(!response.ok){
+                        throw Error('could not fetch the data for that resource')
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    setBlogs(data)
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    setError(err.message);
+                    setIsLoading(false);
+                })
         }, 1000);
     }, []);
 
     return (
         <div className="home">
+            { error && <div> { error } </div> }
             { isLoading && <div>Loading...</div> }
             { blogs && <BlogList blogs={ blogs } title="All Blogs" /> }
         </div>
