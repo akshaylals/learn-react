0ce2384410c8e813cccb0d44b02414db89272def
akshaylals <akshaylal.s@inapp.com>
2022-10-21 10:30:01 +0530

    useEffect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,6 +13,10 @@ const Home = () => {
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log("useEffect ran");
+        console.log(blogs);
+    });
 
     return (
         <div className="home">
